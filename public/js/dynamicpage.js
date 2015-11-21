e1aa5fc7915caccdafb6996a2ffb6201077741f9
John Hannagan <weetbix_@hotmail.com>
2015-11-22 02:09:15 +0800

    Fix history state and add projects base

--- public/js/dynamicpage.js
+++ public/js/dynamicpage.js
@@ -1,43 +1,59 @@
-$(function() {
+$(function () {
     addTitlesBelowImages();
-    
+
     if (Modernizr.history) {
+        
+        history.replaceState({ path: window.location.pathname }, null, null);
+        
         // Override the click event for anchors starting with /
         // This will most likely break things when linking directly
         // to images and such but I'll deal with that later.
-        $("a[href^='/']").click(function(e) {
+        $(document).on("click", "a[href^='/']", function (e) {
             e.preventDefault();
             
             _href = $(this).attr("href");
 
             // change the url without a page refresh and add a history entry.
-            history.pushState(null, null, _href);
+            history.pushState({ path: _href }, null, _href);
 
             // load the content
             loadContent(_href);
+        });
+
+        // We need to handle the popstate event which is called
+        // when the back button is pressed.
+        window.addEventListener('popstate', function (e) {
+            if (e.state) {
+                loadContent(e.state.path);
+            }
         });
 
     }
 });
 
 // Replace main-content div with main-content div from provided url
-function loadContent(href){
+// or the 404 page if not found.
+function loadContent(href) {
     var mainContent = $("#main-content");
-    
-    mainContent.fadeOut(200, function(){
+
+    mainContent.fadeOut(200, function () {
         mainContent.load(href + " #main-content",
-            function(){
+            function (response, status, xhr) {
+                if (status == "error") {
+                    // Page doesnt exist. Show the 404.
+                    mainContent.load("/404.html #main-content");
+                }
+
                 addTitlesBelowImages();
-                mainContent.fadeIn(200);           
-        })
+                mainContent.fadeIn(200);
+            })
     })
 }
 
 // When showing md posts we want to add a span showing the title beneath
 // the image.
-function addTitlesBelowImages()
-{
-    $( "div.post img[title]" ).after(function (){
+function addTitlesBelowImages() {
+    $("div.post img[title]").after(function () {
         return "<span class=\"image-title\">" + this.title + "</span>";
     });
 }
