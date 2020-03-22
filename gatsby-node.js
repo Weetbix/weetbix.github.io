10f5407fa8203a87a8ea6cf46b70ecb6b6df07ea
John Hannagan <weetbix_@hotmail.com>
2020-03-22 15:57:38 +0100

    create pages for projects

---
+++ gatsby-node.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const { createFilePath } = require(`gatsby-source-filesystem`);
+
+// Create nodes for all projects under **/projects/<name>/index.ms
+const createProjectNode = (node, getNode, actions) => {
+  if (node.internal.type === `MarkdownRemark`) {
+    const fileNode = getNode(node.parent);
+
+    const projectFilepathRegex = /projects\/\w+\/index\.md/;
+    const result = fileNode.relativePath.match(projectFilepathRegex);
+
+    if (result) {
+      const slug = createFilePath({ node, getNode, basePath: `pages` });
+      actions.createNodeField({
+        node,
+        name: `slug`,
+        value: slug
+      });
+      actions.createNodeField({
+        node,
+        name: `type`,
+        value: `project`
+      });
+    }
+  }
+};
+
+// Create pages for all projects
+const createProjectPages = async (graphql, actions) => {
+  const result = await graphql(`
+    query getProjectPages {
+      allMarkdownRemark(filter: { fields: { type: { eq: "project" } } }) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    actions.createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/ProjectPage.tsx`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        slug: node.fields.slug
+      }
+    });
+  });
+};
+
+exports.createPages = async ({ graphql, actions }) => {
+  await createProjectPages(graphql, actions);
+};
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  createProjectNode(node, getNode, actions);
+};
---
+++ src/templates/ProjectPage.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import Page from "../components/Page";
+import styled from "styled-components";
+
+const PageContainer = styled.div``;
+
+const ProjectPage: React.FunctionComponent<{}> = () => {
+  return (
+    <Page>
+      <PageContainer>omg</PageContainer>
+    </Page>
+  );
+};
+
+export default ProjectPage;
