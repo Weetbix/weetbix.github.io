4477e21909c7eeabe5f6044badf5578823a23423
John Hannagan <weetbix_@hotmail.com>
2020-03-22 14:19:18 +0100

    Add links to social page

--- src/pages/social.tsx
+++ src/pages/social.tsx
@@ -11,15 +11,36 @@ const PageContainer = styled.div`
   color: #303030;
   padding-left: 35%;
   padding-right: 35%;
+
+  a,
+  a:visited,
+  a:hover,
+  a:active {
+    color: inherit;
+    text-decoration: none;
+    padding-bottom: 3px;
+
+    :hover {
+      color: #ffc785;
+      padding-bottom: 14px;
+      transition: padding 0.1s;
+    }
+  }
 `;
 
 const SocialPage: React.FunctionComponent<{}> = () => {
   return (
     <Page>
       <PageContainer>
-        <FaFlickr size="100px" />
-        <FaGithub size="100px" />
-        <FaInstagram size="100px" />
+        <a href="https://www.flickr.com/photos/77862175@N06/">
+          <FaFlickr size="100px" />
+        </a>
+        <a href="https://github.com/Weetbix">
+          <FaGithub size="100px" />
+        </a>
+        <a href="http://instagram.com/adv_travel">
+          <FaInstagram size="100px" />
+        </a>
       </PageContainer>
     </Page>
   );
