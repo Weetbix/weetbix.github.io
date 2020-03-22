2a279839d4585f438eb1340ff3b4cc7e0520b9ff
John Hannagan <weetbix_@hotmail.com>
2020-03-22 16:04:34 +0100

    basic html loading from project

--- src/templates/ProjectPage.tsx
+++ src/templates/ProjectPage.tsx
@@ -1,15 +1,34 @@
 import * as React from "react";
 import Page from "../components/Page";
 import styled from "styled-components";
+import { ProjectPageQuery } from "../../graphql-types";
 
 const PageContainer = styled.div``;
 
-const ProjectPage: React.FunctionComponent<{}> = () => {
+const ProjectPage: React.FunctionComponent<ProjectPageQuery> = ({ data }) => {
+  const {
+    markdownRemark: { html }
+  } = data;
+
+  console.log(html);
   return (
     <Page>
-      <PageContainer>omg</PageContainer>
+      <PageContainer>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </PageContainer>
     </Page>
   );
 };
+
+export const query = graphql`
+  query ProjectPage($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`;
 
 export default ProjectPage;
