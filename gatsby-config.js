3545898e5b926668332c91ab420b8dc8c126d001
John Hannagan <weetbix_@hotmail.com>
2020-10-24 22:50:04 +0200

    use plausible plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,30 +1,37 @@
 module.exports = {
   siteMetadata: {
-    title: `John Hannagan`
+    title: `John Hannagan`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-styled-components`,
     {
+      resolve: `gatsby-plugin-plausible`,
+      options: {
+        domain: `johnhannagan.com`,
+        customDomain: `plausible.cloud.johnhannagan.com`,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
-        path: `${__dirname}/src/data`
-      }
+        path: `${__dirname}/src/data`,
+      },
     },
     {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-figure-caption`
-          }
-        ]
-      }
+            resolve: `gatsby-remark-figure-caption`,
+          },
+        ],
+      },
     },
     "gatsby-plugin-graphql-codegen",
     "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp"
-  ]
+    "gatsby-plugin-sharp",
+  ],
 };
